47fb51a336366e7a36c59d330f7678a10336efa4
Saydullayev017 <88250263+Saydullayev017@users.noreply.github.com>
2021-10-24 17:10:13 +0300

    Add files via upload

---
+++ const.js
@@ -0,0 +1,29 @@
+const commands = `
+/start - Перезапустить бота
+/help - Помощь
+/course - Сайты
+`
+
+
+const text1 = `
+Мой Youtube Канал =><a href="https://youtube.com/channel/UC9vkOxC3cMYhxGUpFf4zSEQ">Ссылка</a>
+`
+const text2 = `
+Мой Инстаграм =><a href="https://www.instagram.com/saydullayev_017/">Ссылка</a>
+`
+const text3 = `
+Мой ВК =><a href="https://m.vk.com/saydullayev_017">Ссылка</a>
+`
+const text4 = `
+Мой Тик-ток =><a href="www.tiktok.com/@saydullaev_017">Ссылка</a>
+`
+
+
+module.exports.commands = commands
+module.exports.text1 = text1
+module.exports.text2 = text2
+module.exports.text3 = text3
+module.exports.text4 = text4
+
+
+
